4dbd47f6a5466930948f1fb9727d5c819af9e213
root <lukewwwaterhouse@hotmail.co.uk>
2021-12-02 16:07:06 +0000

    reformatted

--- client/src/Components/Entry.js
+++ client/src/Components/Entry.js
@@ -1,45 +1,45 @@
-import React from "react";
-import Register from "./Register";
-import { useState, useEffect } from "react";
-import axios from "axios";
-import Login from "./Login";
+import React from 'react'
+import Register from './Register'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
+import Login from './Login'
 
 function Entry() {
-  const [userName, setUserName] = useState("");
-  const [switchView, setSwitchView] = useState(true);
+  const [userName, setUserName] = useState('')
+  const [switchView, setSwitchView] = useState(true)
 
   function logout() {
     axios
-      .post("http://localhost:5000/logout", {}, { withCredentials: true })
-      .then(() => setUserName(""));
+      .post('http://localhost:5000/logout', {}, { withCredentials: true })
+      .then(() => setUserName(''))
   }
 
   useEffect(() => {
     axios
-      .get("http://localhost:5000/user", { withCredentials: true })
+      .get('http://localhost:5000/user', { withCredentials: true })
       .then((response) => {
-        setUserName(response.data.userName);
-      });
+        setUserName(response.data.userName)
+      })
 
-    logout();
-  }, []);
+    logout()
+  }, [])
 
   function switchViewFunc() {
-    let currentView = switchView;
-    setSwitchView(!currentView);
+    let currentView = switchView
+    setSwitchView(!currentView)
   }
 
   return (
     <div>
-      <button style={{ marginBottom: "5px" }} onClick={() => switchViewFunc()}>
-        <b>Switch</b> Login/Register{" "}
+      <button style={{ marginBottom: '5px' }} onClick={() => switchViewFunc()}>
+        <b>Switch</b> Login/Register{' '}
       </button>
       <div>
         {switchView && <Register />}
         {!switchView && <Login />}
       </div>
     </div>
-  );
+  )
 }
 
-export default Entry;
+export default Entry
--- client/src/Components/Login.js
+++ client/src/Components/Login.js
@@ -1,53 +1,53 @@
-import { useState, useContext } from "react";
-import axios from "axios";
-import UserContext from "./UserContext";
-import "../Css/Register.css";
-import { useHistory } from "react-router-dom";
+import { useState, useContext } from 'react'
+import axios from 'axios'
+import UserContext from './UserContext'
+import '../Css/Register.css'
+import { useHistory } from 'react-router-dom'
 
 function Login() {
-  const [userName, setUserName] = useState("");
-  const [password, setPassword] = useState("");
-  const [passwordError, setPasswordError] = useState(false);
-  const [userNameError, setUserNameError] = useState(false);
+  const [userName, setUserName] = useState('')
+  const [password, setPassword] = useState('')
+  const [passwordError, setPasswordError] = useState(false)
+  const [userNameError, setUserNameError] = useState(false)
 
-  const [isFieldBlank, setIsFieldBlank] = useState(false);
+  const [isFieldBlank, setIsFieldBlank] = useState(false)
 
-  const user = useContext(UserContext);
-  let history = useHistory();
+  const user = useContext(UserContext)
+  let history = useHistory()
 
   function loginUser(e) {
-    e.preventDefault();
+    e.preventDefault()
 
-    if (userName == "" || password == "") {
-      setIsFieldBlank(true);
-      setPasswordError(false);
-      setUserNameError(false);
+    if (userName == '' || password == '') {
+      setIsFieldBlank(true)
+      setPasswordError(false)
+      setUserNameError(false)
     } else {
-      setIsFieldBlank(false);
+      setIsFieldBlank(false)
 
-      const data = { userName, password };
+      const data = { userName, password }
       axios
-        .post("http://localhost:5000/login", data, { withCredentials: true })
+        .post('http://localhost:5000/login', data, { withCredentials: true })
         .then((response) => {
-          console.log(response.data);
-          console.log("Posted!");
+          console.log(response.data)
+          console.log('Posted!')
 
-          if (response.data === "noUserName") {
-            setPasswordError(false);
-            setUserNameError(true);
+          if (response.data === 'noUserName') {
+            setPasswordError(false)
+            setUserNameError(true)
           } else {
-            user.setUserName(response.data.userName);
-            setUserName("");
-            setPassword("");
-            setPasswordError(false);
-            setUserNameError(false);
-            history.push("/homepage");
+            user.setUserName(response.data.userName)
+            setUserName('')
+            setPassword('')
+            setPasswordError(false)
+            setUserNameError(false)
+            history.push('/homepage')
           }
         })
         .catch(() => {
-          setPasswordError(true);
-          setUserNameError(false);
-        });
+          setPasswordError(true)
+          setUserNameError(false)
+        })
     }
   }
 
@@ -58,20 +58,18 @@ function Login() {
       <form action="" onSubmit={(e) => loginUser(e)}>
         <div>
           {isFieldBlank && (
-            <div style={{ color: "red" }}>
-              Please fill out both fields!
-            </div>
+            <div style={{ color: 'red' }}>Please fill out both fields!</div>
           )}
 
           {passwordError && (
-            <div style={{ color: "red" }}>Password incorrect!</div>
+            <div style={{ color: 'red' }}>Password incorrect!</div>
           )}
           {userNameError && (
-            <div style={{ color: "red" }}>Username does not exist!</div>
+            <div style={{ color: 'red' }}>Username does not exist!</div>
           )}
         </div>
 
-        <div style={{ color: "white" }}>Username: </div>
+        <div style={{ color: 'white' }}>Username: </div>
         <input
           type="text"
           id="usernameInput"
@@ -80,7 +78,7 @@ function Login() {
           onChange={(e) => setUserName(e.target.value)}
         />
         <br />
-        <div style={{ color: "white", paddingTop: "10px" }}>Password</div>
+        <div style={{ color: 'white', paddingTop: '10px' }}>Password</div>
         <input
           type="password"
           id="passwordInput"
@@ -91,9 +89,9 @@ function Login() {
         <br />
         <div
           style={{
-            paddingTop: "20px",
-            display: "flex",
-            justifyContent: "center",
+            paddingTop: '20px',
+            display: 'flex',
+            justifyContent: 'center'
           }}
         >
           <button className="btn-1" type="submit">
@@ -102,7 +100,7 @@ function Login() {
         </div>
       </form>
     </div>
-  );
+  )
 }
 
-export default Login;
+export default Login

def6e37775b20ef2ed86208abee680f502451ab7
Your Name <lukewwaterhouse@hotmail.co.uk>
2021-12-04 14:43:40 +0000

    fixed lint errors

--- api/models/Post.js
+++ api/models/Post.js
@@ -1,10 +1,10 @@
-import mongoose from "mongoose";
+import mongoose from 'mongoose'
 var schema = mongoose.Schema({
   userName: { type: String },
   date: { type: String },
-  content: { type: String },
-});
+  content: { type: String }
+})
 
-var Post = mongoose.model("Post", schema);
+var Post = mongoose.model('Post', schema)
 
-export default Post;
+export default Post
--- client/src/Components/Post.js
+++ client/src/Components/Post.js
@@ -1,19 +1,19 @@
-import React from "react";
+import React from 'react'
 
-import Box from "@mui/material/Box";
-import Card from "@mui/material/Card";
-import CardContent from "@mui/material/CardContent";
-import Typography from "@mui/material/Typography";
+import Box from '@mui/material/Box'
+import Card from '@mui/material/Card'
+import CardContent from '@mui/material/CardContent'
+import Typography from '@mui/material/Typography'
 
 function Post(props) {
   return (
     <div
       data-testid="data-postid"
-      style={{ marginRight: "15%", marginLeft: "15%", marginTop: "20px" }}
+      style={{ marginRight: '15%', marginLeft: '15%', marginTop: '20px' }}
     >
       <Box sx={{ minWidth: 275 }}>
         <Card variant="outlined">
-          <CardContent style={{ backgroundColor: "#2C2F33" }}>
+          <CardContent style={{ backgroundColor: '#2C2F33' }}>
             <Typography
               data-testid="data-postdateid"
               sx={{ fontSize: 14 }}
@@ -43,7 +43,7 @@ function Post(props) {
         </Card>
       </Box>
     </div>
-  );
+  )
 }
 
-export default Post;
+export default Post
